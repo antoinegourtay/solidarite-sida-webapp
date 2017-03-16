b0fd95902d928ac0136d341ac3bb103fdd760082
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-03-16 17:13:23 +0100

    [UPDATE] Update entities

--- src/AppBundle/Entity/Affectation.php
+++ src/AppBundle/Entity/Affectation.php
@@ -63,5 +63,119 @@ class Affectation
      */
     private $affectationAdjoint;
 
+    /**
+     * @return int
+     */
+    public function getIdAffectation()
+    {
+        return $this->idAffectation;
+    }
+
+    /**
+     * @param int $idAffectation
+     */
+    public function setIdAffectation($idAffectation)
+    {
+        $this->idAffectation = $idAffectation;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdEquipe()
+    {
+        return $this->idEquipe;
+    }
+
+    /**
+     * @param int $idEquipe
+     */
+    public function setIdEquipe($idEquipe)
+    {
+        $this->idEquipe = $idEquipe;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdDate()
+    {
+        return $this->idDate;
+    }
+
+    /**
+     * @param int $idDate
+     */
+    public function setIdDate($idDate)
+    {
+        $this->idDate = $idDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdEvenement()
+    {
+        return $this->idEvenement;
+    }
+
+    /**
+     * @param int $idEvenement
+     */
+    public function setIdEvenement($idEvenement)
+    {
+        $this->idEvenement = $idEvenement;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdPoste()
+    {
+        return $this->idPoste;
+    }
+
+    /**
+     * @param int $idPoste
+     */
+    public function setIdPoste($idPoste)
+    {
+        $this->idPoste = $idPoste;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdBenevole()
+    {
+        return $this->idBenevole;
+    }
+
+    /**
+     * @param int $idBenevole
+     */
+    public function setIdBenevole($idBenevole)
+    {
+        $this->idBenevole = $idBenevole;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAffectationAdjoint()
+    {
+        return $this->affectationAdjoint;
+    }
+
+    /**
+     * @param bool $affectationAdjoint
+     */
+    public function setAffectationAdjoint($affectationAdjoint)
+    {
+        $this->affectationAdjoint = $affectationAdjoint;
+    }
+
+
+
 
 }
