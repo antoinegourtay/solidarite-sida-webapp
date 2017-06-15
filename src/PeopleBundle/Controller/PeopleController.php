5c9f899aa7537cd238f282357ee19a63a8e6df23
Nicolas Castells <castellsnicolas1303@gmail.com>
2017-06-15 02:14:39 +0200

    [ADD] dashboard import CSV

--- src/PeopleBundle/Controller/PeopleController.php
+++ src/PeopleBundle/Controller/PeopleController.php
@@ -1,6 +1,8 @@
 <?php
 namespace PeopleBundle\Controller;
 
+use PeopleBundle\Entity\People;
+use PeopleBundle\Importer\CSVImporter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -38,5 +40,60 @@ class PeopleController extends Controller
         }
 
         return $this->render('@PeopleBundle/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/import", name="import")
+     * @Method({ "POST" })
+     */
+    public function importAction(Request $request)
+    {
+        if (!$this->get('CurrentUser')->isAuthenticated()) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        if (!$this->get('CurrentUser')->get()->isAdmin()) {
+            return $this->redirectToRoute('dashboard');
+        }
+
+        $csvFile = $request->files->get('csvFile');
+        $people = CSVImporter::import($csvFile->getRealPath());
+
+        foreach($people as $person) {
+            /** @var People $databaseEntry */
+            $databaseEntry = $this->get('PeopleRepository')->getFromEmail($person['email']);
+
+            if (!$databaseEntry) {
+                // CREATE NEW USER TO DATABASE
+                $newPerson = new People();
+                $newPerson->setEmail($person['email']);
+                $newPerson->setFirstName($person['firstName']);
+                $newPerson->setLastName($person['lastName']);
+                $newPerson->setAddress($person['address']);
+                $newPerson->setZipcode($person['zipcode']);
+                $newPerson->setCity($person['city']);
+                $newPerson->setPhone($person['phone']);
+                $newPerson->setDriverLicense($person['driverLicense']);
+            } else {
+                // UPDATE ALREADY EXISTING INFOS
+                $newPerson = $databaseEntry;
+                $newPerson->setFirstName($person['firstName']);
+                $newPerson->setLastName($person['lastName']);
+                $newPerson->setAddress($person['address']);
+                $newPerson->setZipcode($person['zipcode']);
+                $newPerson->setCity($person['city']);
+                $newPerson->setPhone($person['phone']);
+                $newPerson->setDriverLicense($person['driverLicense']);
+            }
+
+            // UPDATE/ADD TEAM
+
+            // SAVE CHANGES
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($newPerson);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('dashboard');
     }
 }
